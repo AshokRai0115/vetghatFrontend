adc702ffb8d0472a7d7e0b3cb2307ea37b361984
AshokRai <bono.ashok@gmail.com>
2023-06-11 18:20:25 +0545

    initial commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
+ 
 navigator.geolocation.watchPosition(success, error);
 
 let marker, circle, zoomed;
